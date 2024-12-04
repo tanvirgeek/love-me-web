93a361d1880b6faa19a37745e4b5f1ed059eed4f
Tanvirgeek <tanvirgeek@gmail.com>
2024-12-05 05:42:00 +0600

    Navbar active done

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,4 +1,6 @@
+"use client";
 import {
+  Button,
   Link,
   Navbar,
   NavbarBrand,
@@ -6,21 +8,16 @@ import {
   NavbarItem,
 } from "@nextui-org/react";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { GiSelfLove } from "react-icons/gi";
 
 const TopNav = () => {
+  const currentPage = usePathname();
+
   return (
     <Navbar
       maxWidth="full"
       className="bg-gradient-to-r from-pink-400 via-red-400 to bg-pink-600"
-      classNames={{
-        item: [
-          "text-xl",
-          "text-white",
-          "uppercase",
-          "data-[active=true]:text-yellow-200",
-        ],
-      }}
     >
       <NavbarBrand as={Link} href="/">
         <GiSelfLove size={40} className="text-gray-200" />
@@ -28,23 +25,77 @@ const TopNav = () => {
           <span className="text-gray-200">Love Me</span>
         </div>
       </NavbarBrand>
-      <NavbarItem>
-        <Link color="foreground" href="/members">
-          Matches
-        </Link>
-      </NavbarItem>
-      <NavbarItem>
-        <Link color="foreground" href="/list">
-          List
-        </Link>
-      </NavbarItem>
-      <NavbarItem>
-        <Link color="foreground" href="/messages">
-          Messages
-        </Link>
-      </NavbarItem>
 
-      <NavbarContent></NavbarContent>
+      <NavbarContent justify="center">
+        <NavbarItem
+          isActive={currentPage === "/members"}
+          className={`${
+            currentPage === "/members" ? "bg-black px-3 py-1 rounded-md" : ""
+          }`}
+        >
+          <Link
+            className={`${
+              currentPage === "/members" ? "text-yellow-300" : "text-white"
+            }`}
+            href="/members"
+            aria-current={currentPage === "/members" ? "page" : undefined}
+          >
+            Matches
+          </Link>
+        </NavbarItem>
+        <NavbarItem
+          isActive={currentPage === "/list"}
+          className={`${
+            currentPage === "/list" ? "bg-black px-3 py-1 rounded-md" : ""
+          }`}
+        >
+          <Link
+            className={`${
+              currentPage === "/list" ? "text-yellow-300" : "text-white"
+            }`}
+            href="/list"
+            aria-current={currentPage === "/list" ? "page" : undefined}
+          >
+            List
+          </Link>
+        </NavbarItem>
+        <NavbarItem
+          isActive={currentPage === "/messages"}
+          className={`${
+            currentPage === "/messages" ? "bg-black px-3 py-1 rounded-md" : ""
+          }`}
+        >
+          <Link
+            className={`${
+              currentPage === "/messages" ? "text-yellow-300" : "text-white"
+            }`}
+            href="/messages"
+            aria-current={currentPage === "/messages" ? "page" : undefined}
+          >
+            Messages
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <Button
+          as={Link}
+          href="/login"
+          variant="bordered"
+          className="text-white"
+        >
+          Login
+        </Button>
+
+        <Button
+          as={Link}
+          href="/register"
+          variant="bordered"
+          className="text-white"
+        >
+          Register
+        </Button>
+      </NavbarContent>
     </Navbar>
   );
 };
