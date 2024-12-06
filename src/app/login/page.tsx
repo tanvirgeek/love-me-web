924928d565b57d2fea2cb716f61ce8b5fb64ddba
Tanvirgeek <tanvirgeek@gmail.com>
2024-12-06 10:16:13 +0600

    Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,12 @@
+import LoginForm from "@/components/auth/loginForm";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default LoginPage;
