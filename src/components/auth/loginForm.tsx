f3094567df29679c763f5ff507df07678f16a713
Tanvirgeek <tanvirgeek@gmail.com>
2024-12-07 06:32:43 +0600

    Login Form Validation with Zod

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -1,8 +1,25 @@
+"use client";
 import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
 import React from "react";
 import { GiPadlock } from "react-icons/gi";
+import { useForm } from "react-hook-form";
+import { loginSchema, LoginSchema } from "@/lib/schemas/LoginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onTouched",
+  });
+
+  const onsubmit = (data: LoginSchema) => {
+    console.log(data);
+  };
+
   return (
     <Card className="w-2/5 mx-auto">
       <CardHeader className="flex flex-col items-center justify-center">
@@ -13,7 +30,7 @@ const LoginForm = () => {
         <p className="text-neutral-500">Welcome back to Love Me</p>
       </CardHeader>
       <CardBody>
-        <form>
+        <form onSubmit={handleSubmit(onsubmit)}>
           <div className="space-y-4">
             <Input
               defaultValue=""
@@ -21,6 +38,9 @@ const LoginForm = () => {
               variant="bordered"
               type="email"
               placeholder="Enter your email"
+              {...register("email")}
+              isInvalid={!!errors.email}
+              errorMessage={errors.email?.message as string}
             />
             <Input
               defaultValue=""
@@ -28,8 +48,16 @@ const LoginForm = () => {
               type="password"
               variant="bordered"
               placeholder="Enter your password"
+              {...register("password")}
+              isInvalid={!!errors.password}
+              errorMessage={errors.password?.message as string}
             />
-            <Button fullWidth type="submit" className="bg-pink-400">
+            <Button
+              fullWidth
+              type="submit"
+              className="bg-pink-400"
+              isDisabled={!isValid}
+            >
               Login
             </Button>
           </div>
